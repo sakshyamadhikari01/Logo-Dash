5ed77942b7d8f585140795a3e349701a38ddd6dd
sakshyamadhikari01 <sakshyamadhikari01@gmail.com>
2026-06-24 00:55:49 +0545

    fvc

--- dashhhh.py
+++ dashhhh.py
@@ -9,6 +9,14 @@ class LogoDashApp(App):
     # Letz use the header title here
     TITLE =  "LOGO DASH"
 
+    #for interested devvs only :D
+
+    #I got inspiration of this game form the games i used to play a long before like mario, geometry dash,
+    #minecraft etc. This game is made for terminal and it works like mario and has character from geometry 
+    #dash ofc to reduce the complexity. Oh yea i used concept from old days ms logo, sheesh brings back memory 
+    #from 5th grade in computer lab. I used commands like fd, jp and since like im using concept from games 
+    #like minecraft and geometry dash i used system of blocks for measurement like fd 3 makes it moves 3 blocks.
+
     CSS = """
     Screen {
         align: center middle;
